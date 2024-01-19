bdf40ccee2579a9766e6d4e2a47ad3cf4b88a0dd
İsmail Kulak <unsubble@gmail.com>
2024-01-19 21:41:35 +0300

    init cmt

---
+++ src/SubbleJSON.java
@@ -0,0 +1,12 @@
+import java.nio.Path;
+import java.nio.Paths;
+
+public final class SubbleJSON {
+	private Path target;
+
+	public SubbleJSON(String path) {
+		target = Paths.get(path);
+	}
+
+	
+}
